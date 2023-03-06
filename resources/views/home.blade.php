2c1362cabde160fcef7340d8cef59cacc08de9b0
Raden616Rizki <rizkiraden616@gmail.com>
2023-03-06 18:35:51 +0700

    delete col

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -122,12 +122,6 @@
           <div class="col-sm-6">
             <h1 class="m-0">Dashboard v2</h1>
           </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="/home">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v2</li>
-            </ol>
-          </div><!-- /.col -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
