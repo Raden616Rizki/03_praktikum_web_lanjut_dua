703ac6097cc77af3289978491690e24f11f1110a
Raden616Rizki <rizkiraden616@gmail.com>
2023-03-07 12:15:47 +0700

    Update home add image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,3 +5,6 @@
         window.alert('Selamat Datang');
     </script>
 @endsection
+@section('content')
+    <img src="https://img.freepik.com/premium-vector/concept-new-team-member_132971-313.jpg?w=1060" style="width: 980px; border-radius: 32px;">
+@endsection
